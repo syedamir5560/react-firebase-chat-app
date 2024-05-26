daeaf8ea5b7b0df76622c58a5b76fc983e6fd55f
syedamir5560 <syedamir5560@gmail.com>
2024-05-26 16:59:15 +0530

    d

--- src/Components/List/ChatList/ChatList.jsx
+++ src/Components/List/ChatList/ChatList.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../ChatList/chatlist.css'
 
 function ChatList() {
+
+    let [addMode,setAddMode]=useState(false)
+
   return (
-    <div className='chatlist'>ChatList</div>
+    <div className='chatlist'>
+        <div className="search">
+            <div className="searchbar">
+                    <img src="./search.png" alt="" />
+                    <input type="text" placeholder='Search'/>
+            </div>
+            <img src={addMode ? "./minus.png" : "./plus.png  "} alt="" className='add'
+            onClick={()=>setAddMode((perv)=>!perv)}
+            />
+        </div>
+        <div className="items">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>John deo</span>
+                <p>Hello</p>
+            </div>
+        </div>
+
+        <div className="items">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>John deo</span>
+                <p>Hello</p>
+            </div>
+        </div>
+
+        <div className="items">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>John deo</span>
+                <p>Hello</p>
+            </div>
+        </div>
+    </div>
   )
 }
 
