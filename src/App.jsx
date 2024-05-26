7f88838bd8c03d5cfc91c9463f1b72a8f5571c33
syedamir5560 <syedamir5560@gmail.com>
2024-05-26 15:40:07 +0530

    list chat details

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import Chat from "./Components/Chat/Chat"
+import Details from "./Components/Details/Details"
+import List from "./Components/List/List"
+
 const App = () => {
   return (
-    <div className=''>App</div>
+    <div className='container'>
+      <List/>
+      <Chat/>
+      <Details/>
+    </div>
   )
 }
 
---
+++ src/Components/Details/Details.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './details.css'
+
+function Details() {
+  return (
+    <div>Details</div>
+  )
+}
+
+export default Details
