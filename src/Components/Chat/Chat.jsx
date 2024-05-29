1c7f18da8210f1814b6a289be0c045f35a104243
syedamir5560 <syedamir5560@gmail.com>
2024-05-29 15:43:54 +0530

    img

--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -40,7 +40,7 @@ function Chat() {
         </div>
 
         <div className="message own">
-          
+
           <div className="texts">
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius
               a autem quaerat inciduntve!
@@ -61,6 +61,7 @@ function Chat() {
 
         <div className="message own">
           <div className="texts">
+            <img src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8fDA%3D" alt="" />
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius
               a autem quaerat inciduntve!
             </p>
--- src/Components/Details/Details.jsx
+++ src/Components/Details/Details.jsx
@@ -3,7 +3,77 @@ import './details.css'
 
 function Details() {
   return (
-    <div className='details'>Details</div>
+    <div className='details'>
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Jane Deo</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Setting</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+
+        <div className="option">
+          <div className="title">
+            <span>Privacy & Help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+
+        <div className="option">
+          <div className="title">
+            <span>Share Photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                <span>photo_2024.png</span>
+              </div>
+              <img src="download.png" alt="" />
+            </div>
+
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                <span>photo_2024.png</span>
+              </div>
+              <img src="download.png" alt="" />
+            </div>
+
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                <span>photo_2024.png</span>
+              </div>
+              <img src="download.png" alt="" />
+            </div>
+
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                <span>photo_2024.png</span>
+              </div>
+              <img src="download.png" alt="" />
+            </div>
+          </div>
+        </div>
+
+        <div className="option">
+          <div className="title">
+            <span>Share Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+      </div>
+    </div>
   )
 }
 
